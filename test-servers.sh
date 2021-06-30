478a8d80ebea9b6b455f237e4fb0a0aecbd1af60
Roman Avseenko <Raman.Auseyenka@itechart-group.com>
2021-06-30 10:35:21 +0300

    Fix

--- test-servers.sh
+++ test-servers.sh
@@ -5,7 +5,7 @@ NAME_VM2="Data"
 
 function vm_test {
   echo "Testing ${1}"
-  python3 -m pytest -v --ansible-inventory="./inventory/aws_ec2.yml" --hosts=${1} --connection=ansible --force-ansible "test-${1}-server.py"
+  python3 -m pytest -v --hosts=${1} --connection=ansible --force-ansible "test-${1}-server.py"
 }
 
 vm_test $NAME_VM1
